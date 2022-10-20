45909bf737c20350bfaf21b7fcf4f1bda8b1eaf4
jd-hyun <wjdehdgus3540@naver.com>
2022-10-20 21:04:39 +0900

    [Feat] JPA를 활용한 DB(repository) 작성 - #12

--- contents/todoListAPI/donghyun/src/main/java/com/example/demo/repository/JpaUserRepository.java
+++ contents/todoListAPI/donghyun/src/main/java/com/example/demo/repository/JpaUserRepository.java
@@ -1,42 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.domain.User;
 
-import javax.persistence.EntityManager;
-import java.util.List;
+import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public class JpaUserRepository implements UserRepository {
+public interface JpaUserRepository extends JpaRepository<User, Long> {
 
-    private EntityManager em;
-
-    public JpaUserRepository(EntityManager em) {
-        this.em = em;
-    }
-    @Override
-    public User save(User user) {
-        em.persist(user);
-        return user;
-    }
-
-    @Override
-    public Optional<User> findById(Long id) {
-        User user = em.find(User.class, id);
-        return Optional.ofNullable(user);
-    }
-
-    @Override
-    public Optional<User> findByEmail(String email) {
-        List<User> result = em.createQuery("select m from User m where m.email = :email", User.class)
-                .setParameter("email", email)
-                .getResultList();
-
-        return result.stream().findAny();
-    }
-
-    @Override
-    public List<User> findAll() {
-        return em.createQuery("select m from User m", User.class)
-                .getResultList();
-    }
+    Optional<User> findByEmail(String email);
 }
