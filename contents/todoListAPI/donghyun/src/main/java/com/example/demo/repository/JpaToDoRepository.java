e05248b63eb9b00125b70a58c4f553b4f565f8da
jd-hyun <wjdehdgus3540@naver.com>
2022-10-20 21:04:39 +0900

    [Feat] JPA를 활용한 DB(repository) 작성 - #12

--- contents/todoListAPI/donghyun/src/main/java/com/example/demo/repository/JpaToDoRepository.java
+++ contents/todoListAPI/donghyun/src/main/java/com/example/demo/repository/JpaToDoRepository.java
@@ -1,33 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.ToDo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
 import java.util.Optional;
 
-public class JpaToDoRepository implements  ToDoRepository {
+public interface JpaToDoRepository extends JpaRepository<ToDo, Long> {
 
-    private EntityManager em;
-
-    public JpaToDoRepository(EntityManager em) {
-        this.em = em;
-    }
-    @Override
-    public ToDo save(ToDo toDo) {
-        em.persist(toDo);
-        return toDo;
-    }
-
-    @Override
-    public Optional<ToDo> findById(Long id) {
-        ToDo toDo = em.find(ToDo.class, id);
-        return Optional.ofNullable(toDo);
-    }
-
-    @Override
-    public List<ToDo> findAll() {
-        return em.createQuery("select m from ToDo m", ToDo.class)
-                .getResultList();
-    }
 }
