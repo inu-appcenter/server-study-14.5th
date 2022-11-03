3c7b75d38d419106571adfd2fb90b642f2e61916
YunJi <76262688+dbswl067@users.noreply.github.com>
2022-11-03 20:15:34 +0900

    [Feat] Swagger 세팅 #32

--- contents/todoListAPI/yoonji/todo-list/src/main/java/com/example/todolist/repository/SpringDataJpaTodoRepository.java
+++ contents/todoListAPI/yoonji/todo-list/src/main/java/com/example/todolist/repository/SpringDataJpaTodoRepository.java
@@ -1,12 +1,8 @@
 package com.example.todolist.repository;
 
-import com.example.todolist.domain.Member;
 import com.example.todolist.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SpringDataJpaTodoRepository extends JpaRepository<Todo, Long> {
 
-    Optional<Todo> findByContent(String content);
 }
